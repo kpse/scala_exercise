15feab405340851c26a5fa04257c8f57d2860fe4
Suo Qin <kpsesuo@gmail.com>
2016-10-19 20:18:29 +1100

    add map2 impl

--- src/test/scala/Chapter4Spec.scala
+++ src/test/scala/Chapter4Spec.scala
@@ -2,12 +2,17 @@ import org.scalactic.TolerantNumerics
 import org.scalatest.FunSpec
 
 class Chapter4Spec extends FunSpec {
+
   trait Option1[+A] {
-    def map[B](f: A => B) : Option1[B]
-    def flatMap[B](f: A => Option1[B]) : Option1[B]
-    def getOrElse[B >: A](default: => B) : B
-    def orElse[B >: A](ob: => Option1[B]) : Option1[B]
-    def filter(f: A => Boolean) : Option1[A]
+    def map[B](f: A => B): Option1[B]
+
+    def flatMap[B](f: A => Option1[B]): Option1[B]
+
+    def getOrElse[B >: A](default: => B): B
+
+    def orElse[B >: A](ob: => Option1[B]): Option1[B]
+
+    def filter(f: A => Boolean): Option1[A]
   }
 
   case class Some1[A](a: A) extends Option1[A] {
@@ -47,18 +52,18 @@ class Chapter4Spec extends FunSpec {
       assert(None1.getOrElse(2) == 2)
 
       assert(Some1(1).map(_ + 1) == Some1(2))
-      assert((None1 : Option1[Int]).map(_ + 1) == None1)
+      assert((None1: Option1[Int]).map(_ + 1) == None1)
 
-      def optionalFunc(a: Int) : Option1[Int] =  if (a > 0) Some1(1) else None1
+      def optionalFunc(a: Int): Option1[Int] = if (a > 0) Some1(1) else None1
 
       assert(Some1(1).flatMap(optionalFunc) == Some1(1))
-      assert((None1 : Option1[Int]).flatMap(optionalFunc) == None1)
+      assert((None1: Option1[Int]).flatMap(optionalFunc) == None1)
 
 
       assert(Some1(1).filter(_ > 1) == None1)
       assert(Some1(1).filter(_ > 0) == Some1(1))
-      assert((None1 : Option1[Int]).filter(_ > 1) == None1)
-      assert((None1 : Option1[Int]).filter(_ > 0) == None1)
+      assert((None1: Option1[Int]).filter(_ > 1) == None1)
+      assert((None1: Option1[Int]).filter(_ > 0) == None1)
 
       assert(Some1(1).orElse(None1) == Some1(1))
       assert(None1.orElse(None1) == None1)
@@ -67,14 +72,27 @@ class Chapter4Spec extends FunSpec {
     }
 
     // 4-2
-    it("should implement variance"){
+    it("should implement variance") {
       implicit val doubleEquality = TolerantNumerics.tolerantDoubleEquality(0.01)
 
       def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.size)
       def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
 
-      assert(variance(Seq(1,1,1)) === Some(0.0))
-      assert(variance(Seq(1,2,3)) === Some(0.6666666666666666))
+      assert(variance(Seq(1, 1, 1)) === Some(0.0))
+      assert(variance(Seq(1, 2, 3)) === Some(0.6666666666666666))
+    }
+
+    // 4-3
+    it("should combine options with map2") {
+      def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+        case (Some(x), Some(y)) => Some(f(x, y))
+        case _ => None
+      }
+
+
+      assert(map2(Some(1), Some(1))(_ + _) == Some(2))
+      assert(map2(Some(1), None)(_ + _) == None)
+      assert(map2(None: Option[Int], None)(_ + _) == None)
     }
 
   }
