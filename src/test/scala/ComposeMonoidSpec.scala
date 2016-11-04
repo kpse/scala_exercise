a246384ac0a65da8e545b47286382721376df5a9
Louis Suo <louis.suo@myob.com>
2016-11-04 15:25:20 +1100

    implement 10-18 bag

--- src/test/scala/ComposeMonoidSpec.scala
+++ src/test/scala/ComposeMonoidSpec.scala
@@ -21,6 +21,7 @@ class ComposeMonoidSpec extends FunSpec {
   }
 
   describe("Monoid") {
+    // 10-16
     it("should compose each other") {
       def productMonoid[A, B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] = new Monoid[(A, B)] {
         override def op(a: (A, B), b: (A, B)): (A, B) = (A.op(a._1, b._1), B.op(a._2, b._2))
@@ -28,12 +29,11 @@ class ComposeMonoidSpec extends FunSpec {
         override def zero: (A, B) = (A.zero, B.zero)
       }
 
-
-
       assert(productMonoid(intAddition, intMultiply).op((1, 2), (3, 4)) == (4, 8))
-
+      assert(productMonoid(intAddition, intAddition).op((1, 2), (3, 4)) == (4, 6))
     }
 
+    // 10-17
     it("should compose function monoid") {
 
       def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] = new Monoid[A => B] {
@@ -50,6 +50,30 @@ class ComposeMonoidSpec extends FunSpec {
 
       assert(op("1") == 3)
       assert(op("5") == 11)
+    }
+
+    def foldMapV[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): B = v.size match {
+      case l if l > 1 => m.op(foldMapV(v.take(l / 2), m)(f), foldMapV(v.drop(l / 2), m)(f))
+      case 1 => f(v.head)
+      case 0 => m.zero
+    }
+
+    // 10-18
+    it("should implement bag") {
+      def bag[A](as: IndexedSeq[A]): Map[A, Int] = {
+        val M: Monoid[Map[A, Int]] = new Monoid[Map[A, Int]] {
+          override def op(a: Map[A, Int], b: Map[A, Int]): Map[A, Int] =
+            (a.keySet ++ b.keySet).foldLeft(zero) { (acc,k) =>
+              acc.updated(k, a.getOrElse(k, 0) + b.getOrElse(k, 0))
+            }
+
+          override def zero: Map[A, Int] = Map()
+        }
+        foldMapV(as, M)((a) => Map(a -> 1))
+      }
+
+      assert(bag(Vector("a", "rose", "is", "a", "rose")) ==  Map("a" -> 2, "rose" -> 2, "is" -> 1))
+      assert(bag(Vector("a", "rose", "is", "a", "rose", "rose")) ==  Map("a" -> 2, "rose" -> 3, "is" -> 1))
     }
 
   }
